e0e3ee15edb382cb48fd75abe8ddb322f825ea81
XHamane <xhamaneparrots@gmail.com>
2024-03-30 04:39:27 +0300

    yyy

---
+++ 1.js
@@ -0,0 +1,11 @@
+// https://discord.com/api/webhooks/1223442391743070350/qenuh9PwwwOBCfp_dCrr4f490HjbXwrU877Zt8TddUGchl8-78yu5ziE1aQfyr-1GuSo
+fetch('https://discord.com/api/webhooks/1223442391743070350/qenuh9PwwwOBCfp_dCrr4f490HjbXwrU877Zt8TddUGchl8-78yu5ziE1aQfyr-1GuSo', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        content: 'Hello, World!',
+        username: 'NEVA EVA BE A NEGA.'
+    })
+})
